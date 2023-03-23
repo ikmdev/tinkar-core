c871dd68412652ac2f88e61460a5653b02fcd006
Purnima Nagpal <punagpal@deloitte.com>
2023-03-23 16:12:57 -0400

    Adding text files in entity folders and updating pom to make Unit test run

--- integration/src/test/java/org/hl7/tinkar/integration/snomed/core/AbstractSnomedTest.java
+++ integration/src/test/java/org/hl7/tinkar/integration/snomed/core/AbstractSnomedTest.java
@@ -1,13 +1,15 @@
 package org.hl7.tinkar.integration.snomed.core;
 
+import org.eclipse.collections.api.list.ImmutableList;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.eclipse.collections.api.list.ImmutableList;
 import org.hl7.tinkar.common.id.PublicId;
 import org.hl7.tinkar.common.service.PrimitiveData;
 import org.hl7.tinkar.common.util.uuid.UuidT5Generator;
-import org.hl7.tinkar.entity.StampEntity;
-import org.hl7.tinkar.entity.StampRecord;
+import org.hl7.tinkar.entity.*;
 import org.hl7.tinkar.terms.State;
+import org.hl7.tinkar.terms.TinkarTerm;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -25,20 +27,62 @@ public abstract class AbstractSnomedTest {
     }
 
     public StampEntity loadStampEntity(JsonNode snomedData) throws IOException {
+
+// TODO Delete this
+//        TinkarTerm.DESCRIPTION_PATTERN.nid();
+//        EntityService.get().nidForUuids(UUID.fromString("a4de0039-2625-5842-8a4c-d1ce6aebf021"));
+
+
         String snomedCtNamespace = snomedData.get("namespace").asText();
         UUID nameSpaceUUID = UuidT5Generator.get(snomedCtNamespace);
         String status = snomedData.get("status").asText();
         State state = State.valueOf(status);
         long effectiveTime = snomedData.get("time").asLong();
-        String moduleId = snomedData.get("module").asText();
+        String module = snomedData.get("module").asText();
         String author = snomedData.get("author").asText();
         String path = snomedData.get("path").asText();
         // TODO how to determine nids for entity
-        StampRecord stampRecord = StampRecord.make(nameSpaceUUID, state, effectiveTime, PrimitiveData.publicId(moduleId), author, path);
-        return stampRecord.stamp();
+
+        UUID authorUuid = UuidT5Generator.get(author);
+        UUID moduleUuid = UuidT5Generator.get(module);
+        UUID pathUuid = UuidT5Generator.get(path);
+
+        ConceptRecord authorConceptRecord = ConceptRecordBuilder.builder()
+                .leastSignificantBits(authorUuid.getLeastSignificantBits())
+                .mostSignificantBits(authorUuid.getMostSignificantBits())
+                .nid(EntityService.get().nidForUuids(authorUuid))
+                .build();
+
+        ConceptRecord moduleConceptRecord = ConceptRecordBuilder.builder()
+                .leastSignificantBits(moduleUuid.getLeastSignificantBits())
+                .mostSignificantBits(moduleUuid.getMostSignificantBits())
+                .nid(EntityService.get().nidForUuids(moduleUuid))
+                .build();
+
+        ConceptRecord pathConceptRecord = ConceptRecordBuilder.builder()
+                .leastSignificantBits(pathUuid.getLeastSignificantBits())
+                .mostSignificantBits(pathUuid.getMostSignificantBits())
+                .nid(EntityService.get().nidForUuids(pathUuid))
+                .build();
+
+        StampVersionRecord stampVersionRecord = StampVersionRecordBuilder.builder()
+                .authorNid(authorConceptRecord.nid())
+                .moduleNid(moduleConceptRecord.nid())
+                .pathNid(pathConceptRecord.nid())
+                .build();
+
+        ImmutableList<StampVersionRecord> stampVersionRecords = new RecordListBuilder<>();
+        stampVersionRecords.newWith(stampVersionRecord);
+        
+        UUID stampUuid = generateStampUuid()
+//        UUID stamp
+        StampRecord stampRecord = StampRecordBuilder.builder()
+                .versions(stampVersionRecords)
+                .leastSignificantBits()
+                .build();
     }
 
-    public UUID getStampUuid(UUID namespaceUuid, String status, long effectiveTime, String moduleId, String author, String path) {
+    public UUID generateStampUuid(UUID namespaceUuid, String status, long effectiveTime, String moduleId, String author, String path) {
         return UuidT5Generator.get(namespaceUuid, status+effectiveTime+moduleId+author+path);
     }
 
