46dcef573bc07ab149f7c68c99cfb1e9b51f4ed8
Purnima Nagpal <punagpal@deloitte.com>
2023-03-23 17:10:26 -0400

    Code refactoring to Abstract tests

--- integration/src/test/java/org/hl7/tinkar/integration/snomed/core/AbstractSnomedTest.java
+++ integration/src/test/java/org/hl7/tinkar/integration/snomed/core/AbstractSnomedTest.java
@@ -3,13 +3,9 @@ package org.hl7.tinkar.integration.snomed.core;
 import org.eclipse.collections.api.list.ImmutableList;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.eclipse.collections.api.list.ImmutableList;
-import org.hl7.tinkar.common.id.PublicId;
-import org.hl7.tinkar.common.service.PrimitiveData;
 import org.hl7.tinkar.common.util.uuid.UuidT5Generator;
 import org.hl7.tinkar.entity.*;
 import org.hl7.tinkar.terms.State;
-import org.hl7.tinkar.terms.TinkarTerm;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,6 +13,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 public abstract class AbstractSnomedTest {
+
     // Utility function to parse and load json file in JsonNode object
     public JsonNode loadJsonFile(String fileName) throws IOException {
         InputStream inputStream = this.getClass().getResourceAsStream(fileName);
@@ -28,62 +25,65 @@ public abstract class AbstractSnomedTest {
 
     public StampEntity loadStampEntity(JsonNode snomedData) throws IOException {
 
-// TODO Delete this
-//        TinkarTerm.DESCRIPTION_PATTERN.nid();
-//        EntityService.get().nidForUuids(UUID.fromString("a4de0039-2625-5842-8a4c-d1ce6aebf021"));
+        // TODO Delete this
+        // Concept - Deloitte's version of Stamp not SNOMED's
 
+        // TinkarTerm.DESCRIPTION_PATTERN.nid();
+        // EntityService.get().nidForUuids(UUID.fromString("a4de0039-2625-5842-8a4c-d1ce6aebf021"));
 
-        String snomedCtNamespace = snomedData.get("namespace").asText();
-        UUID nameSpaceUUID = UuidT5Generator.get(snomedCtNamespace);
+        UUID nameSpaceUUID = UUID.fromString(snomedData.get("namespace").asText());
         String status = snomedData.get("status").asText();
         State state = State.valueOf(status);
         long effectiveTime = snomedData.get("time").asLong();
         String module = snomedData.get("module").asText();
         String author = snomedData.get("author").asText();
         String path = snomedData.get("path").asText();
-        // TODO how to determine nids for entity
 
-        UUID authorUuid = UuidT5Generator.get(author);
-        UUID moduleUuid = UuidT5Generator.get(module);
-        UUID pathUuid = UuidT5Generator.get(path);
+        UUID stampUuid = generateStampUuid(nameSpaceUUID, status, effectiveTime, module, author, path);
 
-        ConceptRecord authorConceptRecord = ConceptRecordBuilder.builder()
-                .leastSignificantBits(authorUuid.getLeastSignificantBits())
-                .mostSignificantBits(authorUuid.getMostSignificantBits())
-                .nid(EntityService.get().nidForUuids(authorUuid))
-                .build();
-
-        ConceptRecord moduleConceptRecord = ConceptRecordBuilder.builder()
-                .leastSignificantBits(moduleUuid.getLeastSignificantBits())
-                .mostSignificantBits(moduleUuid.getMostSignificantBits())
-                .nid(EntityService.get().nidForUuids(moduleUuid))
-                .build();
-
-        ConceptRecord pathConceptRecord = ConceptRecordBuilder.builder()
-                .leastSignificantBits(pathUuid.getLeastSignificantBits())
-                .mostSignificantBits(pathUuid.getMostSignificantBits())
-                .nid(EntityService.get().nidForUuids(pathUuid))
-                .build();
+        // TODO - cleanup: how to determine nids for entity
+        // Everything is a concept & we need to create nids for all concepts to populate in builders
+        ConceptRecord authorConcept = generateConcept(author);
+        ConceptRecord moduleConcept = generateConcept(module);
+        ConceptRecord pathConcept = generateConcept(path);
+        ConceptRecord stateConcept = generateConcept(status);
 
         StampVersionRecord stampVersionRecord = StampVersionRecordBuilder.builder()
-                .authorNid(authorConceptRecord.nid())
-                .moduleNid(moduleConceptRecord.nid())
-                .pathNid(pathConceptRecord.nid())
+                .stateNid(stateConcept.nid())
+                .time(effectiveTime)
+                .authorNid(authorConcept.nid())
+                .moduleNid(moduleConcept.nid())
+                .pathNid(pathConcept.nid())
                 .build();
 
-        ImmutableList<StampVersionRecord> stampVersionRecords = new RecordListBuilder<>();
-        stampVersionRecords.newWith(stampVersionRecord);
-        
-        UUID stampUuid = generateStampUuid()
-//        UUID stamp
+        ImmutableList<StampVersionRecord> stampVersionRecords =
+                RecordListBuilder.make().newWith(stampVersionRecord);
+
         StampRecord stampRecord = StampRecordBuilder.builder()
                 .versions(stampVersionRecords)
-                .leastSignificantBits()
+                .leastSignificantBits(stampUuid.getLeastSignificantBits())
+                .mostSignificantBits(stampUuid.getMostSignificantBits())
                 .build();
+
+        return stampRecord.stamp();
     }
 
     public UUID generateStampUuid(UUID namespaceUuid, String status, long effectiveTime, String moduleId, String author, String path) {
+        // this could have been a random uuid, but to provide more meaningful, uuid - doing it this way
         return UuidT5Generator.get(namespaceUuid, status+effectiveTime+moduleId+author+path);
+    }
+
+    public ConceptRecord generateConcept(String conceptField) {
+        
+        // Creating uuid for all the fields to convert them in concepts.
+        UUID conceptUuid = UuidT5Generator.get(conceptField);
+
+        // this could have been a random uuid, but to provide more meaningful, uuid - doing it this way
+        return ConceptRecordBuilder.builder()
+                .leastSignificantBits(conceptUuid.getLeastSignificantBits())
+                .mostSignificantBits(conceptUuid.getMostSignificantBits())
+                .nid(EntityService.get().nidForUuids(conceptUuid))
+                .build();
     }
 
 }
