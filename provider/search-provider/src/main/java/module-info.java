59ddfa443ebbf8ab2bc83188059ca5771cf62fad
Mahesh Parasnis <mparasnis@tinkarbuild.com>
2023-04-13 12:56:03 -0400

    replacec lucene artifact dependency with lucene-uber.

--- provider/search-provider/src/main/java/module-info.java
+++ provider/search-provider/src/main/java/module-info.java
@@ -2,9 +2,7 @@ module dev.ikm.tinkar.provider.search {
     requires org.slf4j;
     requires transitive dev.ikm.tinkar.entity;
     requires org.eclipse.collections.api;
-    requires org.apache.lucene;
-    requires org.apache.lucene.queryparser;
-    requires org.apache.lucene.search.highlight;
+    requires dev.ikm.jpms.lucene.luceneuber;
     requires static transitive com.google.auto.service;
 
     exports dev.ikm.tinkar.provider.search;
