fc5f7bb1f0725cac370758670b45e1ef6d8689dc
Eric Mays <emays@mays-systems.com>
2024-09-19 17:11:55 -0400

    Initial implementation of hybrid reasoner service

---
+++ reasoner/reasoner-hybrid/src/main/java/dev/ikm/tinkar/reasoner/hybrid/ElkSnomedData.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright © 2015 Integrated Knowledge Management (support@ikm.dev)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dev.ikm.tinkar.reasoner.hybrid;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+import org.eclipse.collections.api.list.primitive.ImmutableIntList;
+import org.eclipse.collections.impl.factory.primitive.IntLists;
+
+import dev.ikm.elk.snomed.model.Concept;
+import dev.ikm.elk.snomed.model.RoleType;
+import dev.ikm.tinkar.common.service.PrimitiveData;
+
+public class ElkSnomedData {
+
+	private final ConcurrentHashMap<Integer, Concept> nidConceptMap = new ConcurrentHashMap<>();
+
+	private final ConcurrentHashMap<Integer, RoleType> nidRoleTypeMap = new ConcurrentHashMap<>();
+
+	private final AtomicInteger activeConceptCount = new AtomicInteger();
+
+	private final AtomicInteger inactiveConceptCount = new AtomicInteger();
+
+	private ImmutableIntList reasonerConceptSet;
+
+	public Collection<Concept> getConcepts() {
+		return Collections.unmodifiableCollection(nidConceptMap.values());
+	}
+
+	public Collection<RoleType> getRoleTypes() {
+		return Collections.unmodifiableCollection(nidRoleTypeMap.values());
+	}
+
+	public RoleType getOrCreateRoleType(int roleNid) {
+		return nidRoleTypeMap.computeIfAbsent(roleNid, RoleType::new);
+	}
+
+	public Concept getConcept(int conceptNid) {
+		return nidConceptMap.get(conceptNid);
+	}
+
+	public Concept getOrCreateConcept(int conceptNid) {
+		return nidConceptMap.computeIfAbsent(conceptNid, Concept::new);
+	}
+
+	public int getActiveConceptCount() {
+		return activeConceptCount.get();
+	}
+
+	public int incrementActiveConceptCount() {
+		return activeConceptCount.incrementAndGet();
+	}
+
+	public int getInactiveConceptCount() {
+		return inactiveConceptCount.get();
+	}
+
+	public int incrementInactiveConceptCount() {
+		return inactiveConceptCount.incrementAndGet();
+	}
+
+	public ImmutableIntList getReasonerConceptSet() {
+		return reasonerConceptSet;
+	}
+
+	public void initializeReasonerConceptSet() {
+		IntStream conceptNids = nidConceptMap.entrySet().stream().mapToInt(es -> (int) es.getKey()).sorted();
+		this.reasonerConceptSet = IntLists.immutable.ofAll(conceptNids);
+	}
+
+	public void writeConcepts(Path path) throws Exception {
+		Files.write(path, nidConceptMap.keySet().stream() //
+				.map(key -> PrimitiveData.publicId(key).asUuidArray()[0] + "\t" + PrimitiveData.text(key)) //
+				.sorted() //
+				.toList());
+	}
+
+	public void writeRoleTypes(Path path) throws Exception {
+		Files.write(path, nidRoleTypeMap.keySet().stream() //
+				.map(key -> PrimitiveData.publicId(key).asUuidArray()[0] + "\t" + PrimitiveData.text(key)) //
+				.sorted() //
+				.toList());
+	}
+
+}
