5c0290fd84f9d372f217c2fee4165cd408b7875b
Eric Mays <emays@mays-systems.com>
2024-10-07 13:13:45 -0400

    Update eclipse collections module in reasoner-hybrid

--- reasoner/reasoner-hybrid/src/main/java/module-info.java
+++ reasoner/reasoner-hybrid/src/main/java/module-info.java
@@ -18,8 +18,8 @@ import dev.ikm.tinkar.reasoner.hybrid.HybridReasonerService;
 import dev.ikm.tinkar.reasoner.service.ReasonerService;
 
 module dev.ikm.tinkar.reasoner.hybrid {
-	requires org.eclipse.collections;
-	requires org.eclipse.collections.api;
+	requires dev.ikm.jpms.eclipse.collections;
+	requires dev.ikm.jpms.eclipse.collections.api;
 	requires org.slf4j;
 
 	requires dev.ikm.tinkar.collection;
