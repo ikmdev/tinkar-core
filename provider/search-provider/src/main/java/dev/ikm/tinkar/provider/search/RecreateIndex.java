7fe5c0209d1007f0eeb136b2528f58eac447cd2e
rbradley813 <rbradley813@gmail.com>
2025-03-20 13:46:35 -0400

    perform load phase with try finally

--- provider/search-provider/src/main/java/dev/ikm/tinkar/provider/search/RecreateIndex.java
+++ provider/search-provider/src/main/java/dev/ikm/tinkar/provider/search/RecreateIndex.java
@@ -58,23 +58,26 @@ public class RecreateIndex extends TrackingCallable<Void> {
         updateProgress(-1,1);
 
         EntityService.get().beginLoadPhase();
-        LongAdder totalEntities = new LongAdder();
-        LongAdder processedEntities = new LongAdder();
-        PrimitiveData.get().forEachParallel((bytes, nid) -> {
-            totalEntities.increment();
-        });
-        updateMessage("Generating Lucene Indexes...");
-        updateProgress(0, totalEntities.longValue()+1);
+        try {
+            LongAdder totalEntities = new LongAdder();
+            LongAdder processedEntities = new LongAdder();
+            PrimitiveData.get().forEachParallel((bytes, nid) -> {
+                totalEntities.increment();
+            });
+            updateMessage("Generating Lucene Indexes...");
+            updateProgress(0, totalEntities.longValue()+1);
 
-        PrimitiveData.get().forEachParallel((bytes, nid) -> {
-            Entity.get(nid).ifPresent(this.indexer::index);
-            processedEntities.increment();
-            if (updateIntervalElapsed()) {
-                updateProgress(processedEntities.longValue(), totalEntities.longValue());
-            }
-        });
-        this.indexer.commit();
-        EntityService.get().endLoadPhase(); // Ending Load Phase triggers type ahead suggester build on background thread
+            PrimitiveData.get().forEachParallel((bytes, nid) -> {
+                Entity.get(nid).ifPresent(this.indexer::index);
+                processedEntities.increment();
+                if (updateIntervalElapsed()) {
+                    updateProgress(processedEntities.longValue(), totalEntities.longValue());
+                }
+            });
+            this.indexer.commit();
+        } finally {
+            EntityService.get().endLoadPhase(); // Ending Load Phase triggers type ahead suggester build on background thread
+        }
         LOG.info("Recreate Lucene Index completed in {}", this.durationString());
         this.updateTitle("Recreate Lucene Index Completed");
         this.updateMessage("Index time: " + this.durationString());
