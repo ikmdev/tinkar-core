e8635df5bd524f10fcfebe2aaf70f88b70840267
Aras <caras@deloitte.com>
2025-04-22 14:18:25 -0400

    changes for test files

--- integration/src/test/java/dev/ikm/tinkar/integration/changeSet/EntityServiceIT.java
+++ integration/src/test/java/dev/ikm/tinkar/integration/changeSet/EntityServiceIT.java
@@ -60,7 +60,7 @@ class EntityServiceIT {
         // Add your assertions here based on the expected summary values
         // For example:
         assertEquals(316, summary.conceptsCount());
-        assertEquals(3063, summary.semanticsCount());
+        assertEquals(3223, summary.semanticsCount());
         assertEquals(16, summary.patternsCount());
         assertEquals(2, summary.stampsCount());
     }
