2ff1e26c7eda9af03962fb2b08fdb3316f0f8cf1
Patrick Chou <1326306+pChou19@users.noreply.github.com>
2026-06-15 14:06:12 -0400

    Remove the service after standalone split

--- service/src/main/java/dev/ikm/tinkar/service/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Tinkar Service Application Package
- * 
- * Main package for the Tinkar Service application providing REST and gRPC APIs
- * for querying Tinkar terminology data.
- */
-package dev.ikm.tinkar.service;
-
